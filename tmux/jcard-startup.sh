2310e4128ae35a9dc5cfa9006982fa0aa4671be9
Mason May <mmay@marqeta.com>
2025-03-10 10:47:37 -0700

    FINAL: fixing the setup script

--- tmux/jcard-startup.sh
+++ tmux/jcard-startup.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 # Define session name
-SESSION="dev-session"
+SESSION="jcard"
 
 # Define directories for each window
-JCARD_DIR="$HOME/projects/jcard"
-JCARD_LOGS_DIR="$HOME/projects/jcard/logs"
-GENERAL_VIM_DIR="$HOME/projects"
+JCARD_DIR="$HOME/source/marqeta-jpos/"
+JCARD_LOGS_DIR="/var/log/jcard/"
+GENERAL_NOTES_DIR="$HOME/notes"
+
+# Kill existing session if it exists
+tmux kill-session -t "$SESSION" 2>/dev/null
 
 # Start a new tmux session and create the first window (jcard)
 tmux new-session -d -s "$SESSION" -n "jcard" -c "$JCARD_DIR"
 
-# Split the jcard window into two vertical panes
-tmux split-window -v -t "$SESSION:jcard"
+# Split the jcard window into two vertical panes and set the correct directory
+tmux split-window -v -t "$SESSION:jcard" -c "$JCARD_DIR"
 
-# In the lower pane, run the Maven command
-tmux send-keys -t "$SESSION:jcard.1" "mvn -pl jcard -am package -DskipTests=true" C-m
+# Send the Maven command to the lower pane but don't block execution
+tmux send-keys -t "$SESSION:jcard.1" "mqc --mq01 --account-name mq01-qa --role-name team-transaction-engine-dev --region-name us-east-1 && mvn -pl jcard -am package -DskipTests=true" C-m
 
 # Create the second window for logs
-tmux new-window -t "$SESSION" -n "jcard logs" -c "$JCARD_LOGS_DIR"
+tmux new-window -t "$SESSION:" -n "jcard logs" -c "$JCARD_LOGS_DIR"
+# The change directory is to get zsh to look correct
+tmux send-keys -t "$SESSION:jcard logs" "cd .. && cd jcard" C-m  # Open Vim in the window
 
 # Create the third window for general Vim
-tmux new-window -t "$SESSION" -n "general vim" -c "$GENERAL_VIM_DIR"
-tmux send-keys -t "$SESSION:general vim" "vim" C-m  # Open Vim in the window
+tmux new-window -t "$SESSION:" -n "general notes" -c "$GENERAL_NOTES_DIR"
 
 # Select the first window as the default
-tmux select-window -t "$SESSION:1"
+tmux select-window -t "$SESSION:jcard.0"
 
 # Attach to session
 tmux attach-session -t "$SESSION"
-
